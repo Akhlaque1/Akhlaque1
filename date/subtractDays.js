bfa7e8811e38a34f9af08f506e5e30c62dccb575
Akhlaque1 <111882302+Akhlaque1@users.noreply.github.com>
2022-09-08 18:44:22 +0530

    Update subtractDays.js

--- date/subtractDays.js
+++ date/subtractDays.js
@@ -11,10 +11,7 @@
  * @returns {Date} the new date with the days subtracted
  * @throws {TypeError} 2 arguments required
  *
- * @example
- * // Subtract 10 days from 1 September 2014:
- * var result = subDays(new Date(2014, 8, 1), 10)
- * //=> Fri Aug 22 2014 00:00:00
+ 
  */
 
 const subtractDays = (dateInput, num) => {
