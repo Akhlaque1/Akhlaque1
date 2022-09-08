8fd8bcccc78163187abb1c178bb07548689b8d37
Akhlaque1 <111882302+Akhlaque1@users.noreply.github.com>
2022-09-08 15:29:36 +0530

    Delete endOfMonth.js

--- date/endOfMonth.js
+++
@@ -1,7 +0,0 @@
-import { endOfMonth as nativeEndOfMonth } from 'date-fns';
-
-import date from './date';
-
-const endOfMonth = (dateInput) => nativeEndOfMonth(date(dateInput));
-
-export default endOfMonth;
